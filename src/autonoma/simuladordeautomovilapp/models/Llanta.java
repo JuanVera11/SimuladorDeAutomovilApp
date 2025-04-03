008d61006437bc3a54ea571360a672832b78840d
Juan Esteban Vera Velez <juanestebanv0612@gmail.com>
2025-04-03 12:07:24 -0500

    Método de acceso y método para calcular el precio de venta según el precio de costo

--- src/autonoma/simuladordeautomovilapp/models/Llanta.java
+++ src/autonoma/simuladordeautomovilapp/models/Llanta.java
@@ -65,4 +65,8 @@ public class Llanta {
     public double getLimiteVelocidad() {
         return limiteVelocidad;
     }
+
+    double getlimiteVelocidad() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
