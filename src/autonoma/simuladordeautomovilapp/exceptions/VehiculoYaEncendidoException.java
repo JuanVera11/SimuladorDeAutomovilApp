891a1903b09ad15afc8a7f115fcf66e908aa551d
Juan Esteban Vera Velez <juanestebanv0612@gmail.com>
2025-04-08 11:29:01 -0500

    Agrego 2 excepciones en carpeta exceptions, VehiculoApagadoOperationException y VehiculoYaEncendidoException

---
+++ src/autonoma/simuladordeautomovilapp/exceptions/VehiculoYaEncendidoException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simuladordeautomovilapp.exceptions;
+
+/**
+ * Excepción cuando el vehiculo esta encendido
+ * @author Juan Esteban Vera Velez
+ * Versión 1.0
+ * since 2025-04-08
+ */
+public class VehiculoYaEncendidoException extends Exception {
+    public VehiculoYaEncendidoException() {
+        super("El vehículo ya está encendido.");
+    }
+}
